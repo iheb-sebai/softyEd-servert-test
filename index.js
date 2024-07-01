fcf64f6402debca99146b8aa0404eca46a95c37a
iheb-sebai <ihebsebai4444@gmail.com>
2024-07-01 14:18:28 +0100

    add paginator

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const paginationMiddleware = require("./paginationMiddleware");
+const paginationMiddleware = require("./paginationMiddleware.js");
 const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
 server.use(middlewares);
